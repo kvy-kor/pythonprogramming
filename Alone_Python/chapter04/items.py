d57da5bf02a6cf8d4285735687a9342f69465f3b
kvy-kor <bjyun30@gmail.com>
2023-06-21 00:01:47 +0900

    update

---
+++ Alone_Python/chapter04/items.py
@@ -0,0 +1,6 @@
+#변수 선언
+example_dictionary = {
+    "키A": "값A",
+    "키A": "값A",
+    "키A": "값A",
+}
---
+++ Alone_Python/chapter04/while_with_time.py
@@ -0,0 +1,13 @@
+#시간과 관련된 기능을 가져옴
+import time
+
+#변수 선언
+number = 0
+
+#5초 동안 반복
+target_tick = time.time() + 5
+while time.time() < target_tick:
+    number += 1
+
+#출력
+print("5초 동안 {}번 반복했습니다.".format(number))
